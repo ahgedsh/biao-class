92a9e855d3be1cec38f3d506209660c63614bd67
ahgedsh <260602815@qq.com>
2018-04-11 15:19:06 +0800

    '1'

---
+++ prictice/030-js-control-flow/main.js
@@ -0,0 +1,86 @@
+
+
+// var age=19;
+// if(age=18){
+//     console.log("kid");
+
+// }else if(age=19){
+//     console.log('老年');
+// }else if(age=20){
+//     console.log("死");
+// }
+// else {
+//     console.log('men');
+// }
+// 
+// var ageElement=document.querySelector('#age');
+// var nameElement=document.querySelector('#name');
+// ageElement.addEventListener('change',function(){
+//     var age=ageElement.value.trim();
+//     age=parseInt(age);
+//     if (!age){
+//         console.log('1');
+//     }else if(age<1){
+//         console.log('2');
+//     }else if(age==18){
+//         console.log('3');
+//     }else{
+//         colsole.log('4');
+//     }
+// })
+
+
+
+
+
+
+
+// nameElement.addEventListener('change', function(){
+//    var  name=nameElement.value.trim();
+//    if(!name){
+//        console.log('请输入名字');
+//    }else{
+//        console.log('你好');
+//    }
+// })
+
+
+// var nameElement=document.querySelector('#name');
+// nameElement.addEventListener('change',function(){
+//     var name=nameElement.value.trim();
+//     switch (name){
+//         case "whh":
+//             colsole.log('你还');
+//             break;
+//         case "lbb":
+//             console.log('hello');
+//             break;
+//         case 'fgg':
+//         console.log('hggj');
+//         case 'jij':
+//         console.log('hg');
+//         break;
+//         case 'fbb':
+//         console.log('huu');
+//     }
+
+    
+// })
+
+
+
+// var phone='18827863321';
+// var name='whh';
+// var age='23';
+// if(!phone){
+//     console.log('号码必须填');
+// }else if(
+//     !phone.startsWith('1')||
+//     phone.length !=11||
+//     !parseInt(phone)
+// ){
+//     console.log('号码格式有误');
+// }else {
+//     console.log('yo');
+// }
+        
